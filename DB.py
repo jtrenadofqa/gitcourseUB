0429763efef8393dc347240ecd4e17eb8002900b
Juan Trenado <juan.trenado@gmail.com>
2025-03-15 13:08:16 +0100

    Added the degree argument to the DB class

--- DB.py
+++ DB.py
@@ -1,7 +1,9 @@
 class DB:
-    def __init__(self, name, address):
+    def __init__(self, name, address, studies="physics"):
         self.name = name
         self.address = address
+        self.studies = studies
 
     def __str__(self):
         return "Name:{}; Address:{}".format(self.name, self.addres)
+
