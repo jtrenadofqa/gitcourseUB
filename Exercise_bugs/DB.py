c932d6889a18a42c477f764750db13b3ab8c448e
Juan <jtrenado@fqa.ub.edu>
2025-03-19 12:23:15 +0100

    Removed clonflicted code

--- Exercise_bugs/DB.py
+++ Exercise_bugs/DB.py
@@ -1,13 +1,7 @@
 class DB:
-    def __init__(self, name, address, studies="physics"):
+    def __init__(self, name, address):
         self.name = name
         self.address = address
-        self.studies = studies
 
     def __str__(self):
-<<<<<<< HEAD
-        return "Name:{}; Address:{}".format(self.name, self.addres)
-
-=======
         return "Name:{}; Address:{}".format(self.name, self.address)
->>>>>>> parent of afa5339 (Added a bug in __str__ in DB.py)
